8552dd67a3122c312d5c7d9710e80d658dc9e0cc
RuchirR02 <ruchirrao04@gmail.com>
2023-03-22 19:43:57 +0530

    login sign backend final

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,request, session, redirect, url_for
 from flask_pymongo import PyMongo
 import pickle
 import numpy as np
+import bcrypt
+import pymongo
 
 popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
@@ -10,9 +12,12 @@ similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/signup_bend'
-mongo = PyMongo(app)
+app.secret_key = "testing"
+client = pymongo.MongoClient("mongodb+srv://ruchirrao04:ruchir03@cluster0.coezmw7.mongodb.net/?retryWrites=true&w=majority")
 
+db = client.get_database('total_records')
+
+records = db.register
 
 @app.route('/')
 def index():
@@ -24,23 +29,95 @@ def index():
                            rating=list(popular_df['avg_rating'].values)
                            )
 
-@app.route('/SignUp', methods= ['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        data= request.form
-        mongo.db.record.insert_one(dict(Name = data['name'],Email= data['email'], Password= data['password']))
-    return render_template('signup.html')
+@app.route("/register", methods=['post', 'get'])
+def register():
+    message = ''
+    
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+    if request.method == "POST":
+        user = request.form.get("fullname")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        user_found = records.find_one({"name": user})
+        email_found = records.find_one({"email": email})
+        if user_found:
+            message = 'There already is a user by that name'
+            return render_template('register.html', message=message)
+        if email_found:
+            message = 'This email already exists in database'
+            return render_template('register.html', message=message)
+        if password1 != password2:
+            message = 'Passwords should match!'
+            return render_template('register.html', message=message)
+        else:
+            #hash the password and encode it
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            #assing them in a dictionary in key value pairs
+            user_input = {'name': user, 'email': email, 'password': hashed}
+            #insert it in the record collection
+            records.insert_one(user_input)
+            
+            #find the new created account and its email
+            user_data = records.find_one({"email": email})
+            new_email = user_data["email"]
+            #if registered redirect to logged in as the registered user
+            return render_template('logged_in.html', email=new_email)
+    return render_template('register.html')
 
-@app.route('/LogIn', methods= ['POST','GET'])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    # users = mongo.db.users
-    # login_user = users.find_one({'email': request.form['email']})
-    # bcrypt = {}
-    # if login_user:
-    #     if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-    #         session['email'] = request.form['email']
-    #         return redirect(url_for('index'))
-    return render_template('signup.html')
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        #check if email exists in database
+        email_found = records.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            #encode the password and check if it matches
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return redirect(url_for('logged_in'))
+            else:
+                if "email" in session:
+                    return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'Email not found'
+            return render_template('login.html', message=message)
+    return render_template('login.html', message=message)
+
+@app.route('/logged_in')
+def logged_in():
+    if "email" in session:
+        email = session["email"]
+        return render_template('logged_in.html', email=email,
+                           book_name = list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values))
+   
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("signout.html")
+    else:
+        return render_template('signout.html')
+
 
 @app.route('/recommend')
 def recommend_ui():
